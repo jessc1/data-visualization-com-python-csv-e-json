5eec6349a3c01771644978e21c65acfb4cef1359
jessc1 <50178378+jessc1@users.noreply.github.com>
2020-04-08 20:13:26 -0300

    Add files via upload

--- highs_lows.py
+++ highs_lows.py
@@ -1,12 +1,36 @@
 import csv
+from matplotlib import pyplot as plt
 
+#Get high temperatures from file.
 filename = 'sitka_weather_07-2014.csv'
 with open (filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     #print(header_row)
 
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+    highs = []
+    for row in reader:
+        highs.append(row[ 1])
+
+    fig = plt.figure(dpi =128, figsize = (10,6))
+    plt.plot(highs, c = 'red')
+
+    #Format plot.
+    plt.title("Daily high temperatures, july 2014", fontsize =24)
+    plt.xlabel('', fontsize = 16)
+    plt.ylabel("Temperature(F)", fontsize = 16)
+    plt.tick_params(axis = 'both', which = 'major', labelsize = 16)
+
+    plt.show()
+
+
+
+    # store the high temperatures from file
+  
+
+    #print(highs)
+
+    #for index, column_header in enumerate(header_row):
+     #   print(index, column_header)"""
 
 
